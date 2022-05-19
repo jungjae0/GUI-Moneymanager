a8df2eb57e2fff9b80c304ad87ed83d25838a0c5
Jung Jaeyoung <93760723+jungjae0@users.noreply.github.com>
2022-05-19 17:27:52 +0900

    Add files via upload

---
+++ 아이디어/개요.py
@@ -0,0 +1,22 @@
+from tkinter import simpledialog
+import tkinter as tk
+from tkinter import ttk
+import csv
+
+class MoneyManage:
+    # 1. __init__
+    # 2. 이름에 따라 csv 파일 열리거나 생성되는 함수
+    # 3. 날짜 삽입 함수
+    # 4. 수입 삽입 함수
+    # 5. 지출 삽입 함수
+    # 6. 항목 삽입 함수
+    # 7. 현재 금액 계산 함수
+    # 8. 현재 금액 삽입 함수
+    # 9. 저장 함수
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
