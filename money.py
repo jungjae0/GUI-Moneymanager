9358bcc18576b018573b2af2d68266aaca954e95
Jung Jaeyoung <93760723+jungjae0@users.noreply.github.com>
2022-06-02 11:43:22 +0900

    Update money.py

--- money.py
+++ money.py
@@ -174,87 +174,88 @@ class Moneymanage:
 
         plt.rcParams['font.family'] = ['NanumGothic', 'sans-serif']
         plt.rcParams['axes.unicode_minus'] = False
-        try:
-            classes = ['식비', '교통비', '쇼핑비', '저축', '문화생활비', '기타']
+        classes = ['식비', '교통비', '쇼핑비', '저축', '문화생활비', '기타']
 
-            f = open(f"{self.user}.csv", encoding='utf-8')
-            lines = f.readlines()
-            outcome = [int(x.split(",")[4]) for x in lines[1:]]
-            category_1 = [str(x.split(",")[5]) for x in lines[1:]]
-            category_2 = ''.join(map(str, category_1))
-            category = category_2.split()
+        f = open(f"{self.user}.csv", encoding='utf-8')
+        lines = f.readlines()
 
-            a_1 = []
-            x = []
-            if "식비" in category:
-                for i in range(len(category)):
-                    if category[i] == "식비":
-                        x.append(i)
-            for i in range(len(x)):
-                a_1.append(outcome[x[i]])
-            a = sum(a_1)
+        outcome = [int(x.split(",")[4]) for x in lines[1:]]
+        category_1 = [str(x.split(",")[5]) for x in lines[1:]]
+        category_2 = ''.join(map(str, category_1))
+        category = category_2.split()
 
-            b_1 = []
-            y = []
-            if "교통비" in category:
-                for i in range(len(category)):
-                    if category[i] == "교통비":
-                        y.append(i)
-            for i in range(len(y)):
-                b_1.append(outcome[y[i]])
-            b = sum(b_1)
+        a_1 = []
+        x = []
+        if "식비" in category:
+            for i in range(len(category)):
+                if category[i] == "식비":
+                    x.append(i)
+        for i in range(len(x)):
+            a_1.append(outcome[x[i]])
+        a = sum(a_1)
 
-            c_1 = []
-            z = []
-            if "문화생활비" in category:
-                for i in range(len(category)):
-                    if category[i] == "문화생활비":
-                        z.append(i)
-            for i in range(len(z)):
-                c_1.append(outcome[z[i]])
-            c = sum(c_1)
+        b_1 = []
+        y = []
+        if "교통비" in category:
+            for i in range(len(category)):
+                if category[i] == "교통비":
+                    y.append(i)
+        for i in range(len(y)):
+            b_1.append(outcome[y[i]])
+        b = sum(b_1)
 
-            d_1 = []
-            l = []
-            if "쇼핑비" in category:
-                for i in range(len(category)):
-                    if category[i] == "쇼핑비":
-                        l.append(i)
-            for i in range(len(l)):
-                d_1.append(outcome[l[i]])
-            d = sum(d_1)
+        c_1 = []
+        z = []
+        if "문화생활비" in category:
+            for i in range(len(category)):
+                if category[i] == "문화생활비":
+                    z.append(i)
+        for i in range(len(z)):
+            c_1.append(outcome[z[i]])
+        c = sum(c_1)
 
-            e_1 = []
-            m = []
-            if "저축" in category:
-                for i in range(len(category)):
-                    if category[i] == "저축":
-                        m.append(i)
-            for i in range(len(m)):
-                e_1.append(outcome[m[i]])
-            e = sum(e_1)
+        d_1 = []
+        l = []
+        if "쇼핑비" in category:
+            for i in range(len(category)):
+                if category[i] == "쇼핑비":
+                    l.append(i)
+        for i in range(len(l)):
+            d_1.append(outcome[l[i]])
+        d = sum(d_1)
 
-            f_1 = []
-            n = []
-            if "기타" in category:
-                for i in range(len(category)):
-                    if category[i] == "기타":
-                        n.append(i)
-            for i in range(len(n)):
-                f_1.append(outcome[n[i]])
-            f = sum(f_1)
+        e_1 = []
+        m = []
+        if "저축" in category:
+            for i in range(len(category)):
+                if category[i] == "저축":
+                    m.append(i)
+        for i in range(len(m)):
+            e_1.append(outcome[m[i]])
+        e = sum(e_1)
 
-            slices = [a, b, c, d, e, f]
+        f_1 = []
+        n = []
+        if "기타" in category:
+            for i in range(len(category)):
+                if category[i] == "기타":
+                    n.append(i)
+        for i in range(len(n)):
+            f_1.append(outcome[n[i]])
+        f = sum(f_1)
 
-            colors = ['lightblue', 'green', 'orange', 'Yellow', 'gold', 'pink']
+        slices = [a, b, c, d, e, f]
 
+        colors = ['lightblue', 'green', 'orange', 'Yellow', 'gold', 'pink']
+
+        if sum(slices) != 0:
             plt.pie(slices, autopct='%2.3f%%', colors=colors, labels=classes)
 
             plt.legend(loc=(1, 0.7))
             plt.show()
+            
+        self.txt_result.config(text="Faile : Data is not available.", fg="red")
 
-        except ValueError:
-            self.txt_result.config(text="Faile : Data is not available.", fg="red")
 
 
     def clear(self):
